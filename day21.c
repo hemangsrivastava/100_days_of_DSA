06e85d1f692dc50c81b423e142fb27dc93f29b8c
Shashwat Kashyap <hemangsrivastava.contact@gmail.com>
2026-02-21 12:00:00 +0530

    Add day21.c (Day 21)

---
+++ day21.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  int data;
+  struct Node *next;
+}node;
+
+node* createnode(int value){
+  node* newnode = (node*)malloc(sizeof(node));
+  if (!newnode){
+    printf("Memory allocation failed");
+    exit(1);
+  }
+  newnode->data = value;
+  newnode->next = NULL;
+  return newnode;
+}
+
+void insertAtBeginning(node** head, int value){
+  node* newnode = createnode(value);
+  newnode -> next = *head;
+  *head = newnode;
+  printf("inserted node at beginning.\n");
+}
+
+void printlist(node * head){
+  if (head == NULL){
+    printf("the list is empty");
+    return;
+  }
+  node * temp = head;
+  printf("Linked List :");
+  while (temp!=NULL){
+    printf("%d ->", temp->data);
+    temp = temp->next;
+  }
+  printf("NULL\n");
+}
+
+int main(){
+  node* head = NULL;
+  insertAtBeginning(&head, 18);
+  insertAtBeginning(&head, 3);
+  insertAtBeginning(&head, 1);
+  printlist(head);
+}
