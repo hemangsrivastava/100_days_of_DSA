92b8d885bc2f32c858d2f1c64f06321a89b85b60
Shashwat Kashyap <hemangsrivastava.contact@gmail.com>
2026-04-17 12:00:00 +0530

    Add day76.c (Day 76)

---
+++ day76.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100005
+
+int visited[MAX];
+int *adj[MAX];
+int adjSize[MAX];
+
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 0; i < adjSize[node]; i++) {
+        int neighbor = adj[node][i];
+        if (!visited[neighbor]) {
+            dfs(neighbor);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 1; i <= n; i++) {
+        adj[i] = malloc(sizeof(int) * (n + 1));
+        adjSize[i] = 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        adj[u][adjSize[u]++] = v;
+        adj[v][adjSize[v]++] = u;
+    }
+
+    int components = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
